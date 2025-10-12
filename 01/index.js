c20bdc8c210e5a42a36f1b4991499ce7f6e6550f
Md Noorullah Raza <mnraza94@gmail.com>
2025-10-12 23:46:22 +0530

    Refactor chat messages in OpenAI integration for improved user interaction

--- 01/index.js
+++ 01/index.js
@@ -2,20 +2,26 @@ import OpenAI from "openai";
 import "dotenv/config";
 
 const openai = new OpenAI({
-  apiKey: process.env.GEMINI_API_KEY,
-  baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+    apiKey: process.env.GEMINI_API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
 });
 
 async function main() {
-  const response = await openai.chat.completions.create({
-    model: "gemini-2.0-flash",         
-    messages: [
-      { role: "system", content: "You are a helpful assistant." },
-      { role: "user", content: "hey, how are you ?" }
-    ],    
-  });
+    const response = await openai.chat.completions.create({
+        model: "gemini-2.0-flash",
+        messages: [
+            { role: "user", content: "hey, My name is MN Raza." },
+            {
+                role: "assistant",
+                content: "Okay, MN Raza. Nice to meet you! How can I help you today?",
+            }, {
+                role: "user",
+                content: "What is my name ?",
+            }
+        ],
+    });
 
-  console.log(response.choices[0].message.content);
+    console.log(response.choices[0].message.content);
 }
 
 main()
